735ffef4bc401830f694a14b08c3c3c70052f676
DESKTOP-JCHF5KA\black <tranngocminh16294@gmail.com>
2021-10-14 17:37:30 +0700

    add bootstrap, fix owl carousel

--- app/design/frontend/MinhTran/CustomTheme/Magento_Theme/web/requirejs-config.js
+++ app/design/frontend/MinhTran/CustomTheme/Magento_Theme/web/requirejs-config.js
@@ -2,9 +2,25 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+ 
 var config = {
     deps: [
         'Magento_Theme/js/theme'
     ]
+};
+var config = {
+    paths: {
+        'bootstrap': [
+            'Magento_Theme/js/bootstrap.bundle',
+            'Magento_Theme/js/jquery-3.2.1.slim.min.js',
+            'Magento_Theme/js/popper.min.js',
+
+        ]
+
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
 };
